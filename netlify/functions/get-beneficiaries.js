cd66c7b3526fb4503573abdc26bd954641c3ce43
Tadiwalawson <charilaw21@gmail.com>
2026-08-25 23:35:50 +0200

    Added Jah Master (Rodney Mashandure) - Mercedes-Benz C200

--- netlify/functions/get-beneficiaries.js
+++ netlify/functions/get-beneficiaries.js
@@ -36,8 +36,9 @@ exports.handler = async (event) => {
     { name: "Diana Samkange", role: "Musician", gift: "Mercedes Benz" },
     { name: "Peter Moyo", role: "Musician (son of Tongai Moyo)", gift: "Mercedes Benz C Class" },
     { name: "Shinsoman", role: "Zimdancehall Musician", gift: "Mercedes-Benz C-Class (US$25,000) plus US$10,000 cash for fuel and personal use" },
-{ name: "Mambo Dhuterere", role: "Gospel Musician", gift: "White Toyota Fortuner 2.8 GD6 plus US$50,000 cash" },
-{ name: "Mai Dhuterere", role: "Gospel Musician", gift: "White Toyota Fortuner 2.8 GD6 plus US$100,000 cash" },
+    { name: "Mambo Dhuterere", role: "Gospel Musician", gift: "White Toyota Fortuner 2.8 GD6 plus US$50,000 cash" },
+    { name: "Mai Dhuterere", role: "Gospel Musician", gift: "White Toyota Fortuner 2.8 GD6 plus US$100,000 cash" },
+    { name: "Jah Master", role: "Zimdancehall Musician", gift: "Mercedes-Benz C200 (April 2024) for his hit song 'Hello Mwari' and performing at ZANU PF rallies" },
 
     // ============================================================
     // COMEDIANS & CONTENT CREATORS
